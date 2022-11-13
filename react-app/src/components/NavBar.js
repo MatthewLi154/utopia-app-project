807a7c3bb4351e72d1076511ecc2ac6281918b39
William Ngo <williamkimngo@gmail.com>
2022-11-12 22:25:44 -0800

    loginmodal

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -5,9 +5,11 @@ import LogoutButton from './auth/LogoutButton';
 import LoginFormModal from './auth/LoginFormModal';
 import SignUpFormModal from './auth/SignUpFormModal';
 import { useSelector } from 'react-redux';
+import LoginForm from './auth/LoginForm';
+import { Modal } from '../context/Modal';
 import './NavBar.css'
 
-const NavBar = ({ isLoaded }) => {
+const NavBar = ({ loaded }) => {
   const sessionUser = useSelector(state => state.session.user)
   const [signup, setShowSignup] = useState(false)
   const [login, setLogin] = useState(false)
@@ -19,7 +21,7 @@ const NavBar = ({ isLoaded }) => {
         <ul className='nav-list'>
           <div className='left-nav-items'>
             <li className='left-nav'>
-              <NavLink  to='/' exact={true}>
+              <NavLink to='/' exact={true}>
                 <img src='https://i.imgur.com/TTcEnHK.png' alt='logo' className='home-logo'></img>
               </NavLink>
               <NavLink className="discover" to='/profiles' exact={true}>
@@ -31,15 +33,15 @@ const NavBar = ({ isLoaded }) => {
             </li>
           </div>
 
-        <div className='Right-nav-user'>
-          <Link  to='/profiles'>
-            <img  src='https://i.imgur.com/1kY4QtL.png' alt="default-profile-pic" className='default-profile-pic'></img>
-          </Link>
-          <div className='user-name'>Matthew Li</div>
-        <div>
-          <LogoutButton />
-        </div>
-        </div>
+          <div className='Right-nav-user'>
+            <Link to='/profiles'>
+              <img src='https://i.imgur.com/1kY4QtL.png' alt="default-profile-pic" className='default-profile-pic'></img>
+            </Link>
+            <div className='user-name'>Matthew Li</div>
+            <div>
+              <LogoutButton />
+            </div>
+          </div>
         </ul>
       </div>
     )
@@ -47,46 +49,56 @@ const NavBar = ({ isLoaded }) => {
     sessionLinks = (
       <div>
         <div className='nav-bar-splash'>
-        <ul className='nav-list-splash'>
-          <div className='left-logo'>
-            <li>
-              <NavLink to='/' exact={true}>
-                <img src='https://i.imgur.com/1kY4QtL.png' alt='logo' className='home-logo'></img>
-              </NavLink>
-              </li>
+          <div className='nav-list-splash'>
+            <div className='left-logo'>
+              <div>
+                <NavLink to='/' exact={true}>
+                  <img src='https://i.imgur.com/1kY4QtL.png' alt='logo' className='home-logo'></img>
+                </NavLink>
               </div>
+            </div>
 
-      <div className='right-nav-items-splash'>
-        <li>
-          <NavLink to='/login' exact={true} className="login">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} className="signup">
-            Sign Up
-          </NavLink>
-        </li>
+            <div className='right-nav-items-splash'>
+              <div>
+                <button className="login-button" onClick={() => {
+                  console.log(login)
+                  setLogin(true)
+                  console.log(login)
+                }
+                }>Log In</button>
+              </div>
+              <div>
+                <NavLink to='/sign-up' exact={true} className="signup">
+                  Sign Up
+                </NavLink>
+              </div>
+            </div>
+          </div>
         </div>
-        </ul>
-      </div>
+
+
       </div >
     )
   }
-return (
-  <div>
-    {sessionLinks}
-  </div>
+  return (
+    <div>
+      {loaded && sessionLinks}
 
-  // <div>
-  //   <div>
-  //     <LogoutButton />
-  //   </div>
-  //   <div>
-  //     {isLoaded && sessionLinks}
-  //   </div>
-  // </div>
-);
+        {login && <Modal onClose={() => setLogin(false)}>
+          <LoginForm setLogin={setLogin}/>
+        </Modal>}
+
+    </div>
+
+    // <div>
+    //   <div>
+    //     <LogoutButton />
+    //   </div>
+    //   <div>
+    //     {isLoaded && sessionLinks}
+    //   </div>
+    // </div>
+  );
 }
 
 export default NavBar;
--- react-app/src/components/auth/LoginFormModal.js
+++ react-app/src/components/auth/LoginFormModal.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal } from '../../context/Modal';
 import LoginForm from "./LoginForm";
 
-function LoginFormModal({setLogin, login}) {
-
+function LoginFormModal() {
+    const [login, setLogin] = useState(false)
     return (
         <>
-        {login && (
+        <button className="login-button" onClick={() =>
+          {console.log(login)
+            setLogin(true)
+          console.log(login)}
+          }>Log In</button>
+        {(
             <Modal onClose={() => setLogin(false)}>
-                <LoginForm setLogin={setLogin}/>
+                <LoginForm />
             </Modal>
         )}
         </>
