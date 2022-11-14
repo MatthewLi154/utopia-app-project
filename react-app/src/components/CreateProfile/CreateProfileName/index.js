860fbd7a991a7e238c0bcce18c0516d4bfe3babc
MatthewLi154 <matthewli154@MATTS_IPHONE.localdomain>
2022-11-14 09:15:59 -0800

    adjusted routing for create profile

--- react-app/src/components/CreateProfile/CreateProfileName/index.js
+++ react-app/src/components/CreateProfile/CreateProfileName/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useHistory, useDispatch } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink, useHistory } from "react-router-dom";
+import "./CreateProfileName.css";
 
 function CreateProfileName() {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const [name, setName] = useState("");
   const [validationErrors, setValidationErrors] = useState("");
@@ -21,25 +23,59 @@ function CreateProfileName() {
   };
 
   return (
-    <div>
-      <h1>What is your name</h1>
-      <label>Name</label>
-      <form>
-        <input
-          placeholder="First name, Last name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </form>
-      <NavLink
-        to={{
-          pathname: "/profile/create/birthday",
-          state: { newProfile: newProfile },
-        }}
-      >
-        <button onClick={(e) => onSubmit(e)}>Next</button>
-      </NavLink>
-    </div>
+    <>
+      <div className="create-name-main-page-container">
+        <div className="create-name-header-container">
+          <div className="create-name-center-content">
+            <div className="create-name-icon-details">
+              <div
+                className="create-name-icon"
+                onClick={() => {
+                  history.push("/profiles");
+                }}
+              >
+                <i className="fa-solid fa-angle-left"></i>
+              </div>
+              <div className="create-name-icon">
+                <i className="fa-solid fa-file-signature"> </i>
+              </div>
+              <div className="create-name-details">Who are you?</div>
+            </div>
+          </div>
+        </div>
+        <div className="create-name-bottom-section-details-container">
+          <div className="input-name-container">
+            <div>
+              <label>Name</label>
+            </div>
+            <form>
+              <input
+                placeholder="John Smith"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </form>
+          </div>
+          <div className="create-name-button-container">
+            <div className="create-name-button-sub-container">
+              <NavLink
+                to={{
+                  pathname: "/profile/create/birthday",
+                  state: { newProfile: newProfile },
+                }}
+              >
+                <button
+                  className="create-name-button"
+                  onClick={(e) => onSubmit(e)}
+                >
+                  Next
+                </button>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
