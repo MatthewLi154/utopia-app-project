c248a3073079751339bd67141fb569c88fa50920
William Ngo <williamkimngo@gmail.com>
2022-11-13 18:11:41 -0800

    added seeder profiles

--- app/seeds/profiles.py
+++ app/seeds/profiles.py
@@ -4,7 +4,7 @@ from app.models.profile import Profile
 # Adds a profile for seed users
 def seed_profiles():
     demo_profile = Profile(
-        user_id=1, first_name='Demo', last_name='User', birthday='11-20-1993', location='Mars, Milky Way', bio='This is a demo bio!', identify_as='Human', looking_for='Alien', img_url='image.jpg', score=12
+        user_id=1, first_name='Demo', last_name='User', birthday='11-20-1993', location='Mars, Milky Way', bio='This is a demo bio!', identify_as='Human', looking_for='Anime', img_url='image.jpg', score=12
     )
 
     demo_profile_2 = Profile(
@@ -12,27 +12,32 @@ def seed_profiles():
     )
 
     demo_profile_3 = Profile(
-        user_id=3, first_name='Dion', last_name='Pham', birthday='03-05-1999', location='Torrance, CA',  bio="Gym Addict who loves good vibes", identify_as='Human', looking_for='Human', img_url='image3.jpg', score=14
+        user_id=3, first_name='Dion', last_name='Pham', birthday='03-05-1999', location='Torrance, CA',  bio="Gym Addict who loves good vibes", identify_as='Human', looking_for='Vampire', img_url='image3.jpg', score=15
     )
-    demo_profile_3 = Profile(
-        user_id=3, first_name='Bobbie', last_name='Ross', birthday='03-05-2001', location='Pasadena, CA',  bio="This is Bobbies' bio!", identify_as='Raccoon', looking_for='Honey Badger', img_url='image3.jpg', score=14
+    demo_profile_4 = Profile(
+        user_id=3, first_name='Tinker', last_name='Bell', birthday='03-05-1953', location='Neverland',  bio="A sassy fairy that is tired of playing sidekick.", identify_as='Fairy', looking_for='Wizard', img_url='image3.jpg', score=16
     )
-    demo_profile_3 = Profile(
-        user_id=3, first_name='Bobbie', last_name='Ross', birthday='03-05-2001', location='Pasadena, CA',  bio="This is Bobbies' bio!", identify_as='Raccoon', looking_for='Honey Badger', img_url='image3.jpg', score=14
+    demo_profile_5 = Profile(
+        user_id=3, first_name='Shrek', last_name='Ross', birthday='03-05-2001', location='Swamp',  bio="There is more to me than your average onion", identify_as='Ogre', looking_for='Princess', img_url='image3.jpg', score=17
     )
-    demo_profile_3 = Profile(
-        user_id=3, first_name='Bobbie', last_name='Ross', birthday='03-05-2001', location='Pasadena, CA',  bio="This is Bobbies' bio!", identify_as='Raccoon', looking_for='Honey Badger', img_url='image3.jpg', score=14
+    demo_profile_6 = Profile(
+        user_id=3, first_name='Severus', last_name='Snape', birthday='01-09-1960', location='Hogwarts',  bio="Introverted, emotional, nerd and a one-woman man", identify_as='Wizard', looking_for='Fairy', img_url='image3.jpg', score=18
     )
-    demo_profile_3 = Profile(
-        user_id=3, first_name='Bobbie', last_name='Ross', birthday='03-05-2001', location='Pasadena, CA',  bio="This is Bobbies' bio!", identify_as='Raccoon', looking_for='Honey Badger', img_url='image3.jpg', score=14
+    demo_profile_7 = Profile(
+        user_id=3, first_name='Princess', last_name='Peach', birthday='01-08-1965', location="'Bowser's Castle'",  bio="Like's Cart racing, dislikes banana peels", identify_as='Princess', looking_for='Ogre', img_url='image3.jpg', score=19
     )
-    demo_profile_3 = Profile(
-        user_id=3, first_name='Bobbie', last_name='Ross', birthday='03-05-2001', location='Pasadena, CA',  bio="This is Bobbies' bio!", identify_as='Raccoon', looking_for='Honey Badger', img_url='image3.jpg', score=14
+    demo_profile_8 = Profile(
+        user_id=3, first_name='Tsunade', last_name='Senju', birthday='03-05-2001', location='Konoha',  bio="5th Hokage, need I say more", identify_as='anime', looking_for='Human', img_url='image3.jpg', score=20
     )
 
     db.session.add(demo_profile)
     db.session.add(demo_profile_2)
     db.session.add(demo_profile_3)
+    db.session.add(demo_profile_4)
+    db.session.add(demo_profile_5)
+    db.session.add(demo_profile_6)
+    db.session.add(demo_profile_7)
+    db.session.add(demo_profile_8)
     db.session.commit()
 
 def undo_profiles():
